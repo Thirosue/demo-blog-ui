900b21289695b735a3db7758dca058b7c0ddc0c3
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2018-02-01 12:00:39 +0900

    first commit

--- src/main/java/com/example/blog/UiController.java
+++ src/main/java/com/example/blog/UiController.java
@@ -4,6 +4,7 @@ import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,29 +14,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
+
 @Controller
 public class UiController {
-	private final WebClient webClient;
-	private final Logger log = LoggerFactory.getLogger(UiController.class);
+    private final WebClient webClient;
+    private final Logger log = LoggerFactory.getLogger(UiController.class);
 
-	public UiController(WebClient.Builder builder, BlogProperties props) {
-		this.webClient = builder.baseUrl(props.getApi().getUri()).build();
-	}
+    public UiController(WebClient.Builder builder, BlogProperties props) {
+        this.webClient = builder.baseUrl(props.getApi().getUri()).build();
+    }
 
-	@GetMapping(path = "/")
-	public String index(Model model) {
-		log.info("index");
-		Flux<Entry> entries = Flux.empty(); // TODO: 演習3
-		model.addAttribute("entries", entries);
-		return "index";
+    @GetMapping(path = "/")
+    public String index(Model model) {
+        log.info("index");
+        Flux<Entry> entries = webClient.get()
+                .uri("/v1/entries").accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Entry.class);
+        model.addAttribute("entries", entries);
+        return "index";
 
-	}
+    }
 
-	@GetMapping(path = "entries/{entryId}")
-	public String entry(@PathVariable EntryId entryId, Model model) {
-		log.info("entry");
-		Mono<Entry> entry = Mono.empty(); // TODO: 演習3
-		model.addAttribute("entry", entry);
-		return "entry";
-	}
+    @GetMapping(path = "entries/{entryId}")
+    public String entry(@PathVariable EntryId entryId, Model model) {
+        log.info("entry");
+        Mono<Entry> entry = webClient.get()
+                .uri("/v1/entries/{entryId}", entryId).accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Entry.class);
+        model.addAttribute("entry", entry);
+        return "entry";
+    }
 }
